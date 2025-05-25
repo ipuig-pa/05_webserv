d4f6af31413b602e55a04f7040127be14b1c8115
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-05-25 16:43:51 +0200

    instructions with commands and some test cgi/static files

--- www/cgi/post.py
+++ www/cgi/post.py
@@ -9,5 +9,3 @@ data = sys.stdin.read(length)
 
 print("POST Content from client is: ")
 print(data)
-
-# command: curl -X POST -d "POST content evalutor want to test" http://127.0.0.1:8080/www/cgi/post.py
---
+++ www/cgi/todelete.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python3
+
+print("Content-Type: text/html\n")
+print()
+print("<html><body><h1>you can delete me!</h1></body></html>\n")
